4a3340949bd703e556548b926dc989a04b8a9af3
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-10 23:10:25 +0000

    Automatically backed up by Learn

--- lib/concerns/modules.rb
+++ lib/concerns/modules.rb
@@ -1,3 +1,13 @@
 module Concerns::Modules
   
+  def find_by_name(name)
+    @@all.select do |song|
+      song.name == name
+    end
+  end
   
+  def fine_or_create_by_name(name)
+    find_by_name(name) || self.create(name)
+  end
+  
+end
