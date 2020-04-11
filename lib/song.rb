6a780155d20f5e6bd898958a47e0545ca8efc9e5
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-11 00:58:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,8 @@ class Song
   end
   
   def self.create_from_filename(file)
-    self.new_from_filename(file)
+    song = self.new_from_filename(file)
+    song.save
   end
   
   def self.find_by_name(name)
