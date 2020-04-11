42eb0bd48f55af4f59b89a0767eb5b0f07017d92
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-11 02:03:35 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,7 +26,16 @@ class MusicLibraryController
     elsif input = "list songs"
       list_songs
     elsif input = "list artists"
-      
+      list_artists
+    elsif input = "list genres"
+      list_genres
+    elsif input = "list songs by artist"
+      list_songs_by_artist
+    elsif input = "list songs by genre"
+      list_songs_by_genre
+    elsif input = "play song"
+      play_song
+    end
   end
   
   def list_songs
