2dfbdc8093f52823737e665710384814da6d2a71
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-11 01:50:47 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,4 +9,29 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
   end
+  
+  def list_songs
+    
+  end
+  
+  def list_artists
+    
+  end
+  
+  def list_genres
+    
+  end
+  
+  def list_songs_by_artist
+    
+  end
+  
+  def list_songs_by_genre
+    
+  end
+  
+  def play_song
+    
+  end
+  
 end
